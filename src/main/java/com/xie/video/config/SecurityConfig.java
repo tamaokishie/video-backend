3e628951026bea1c315e326589cb9bc4042da679
ShieTamaoki <shie0041@gmail.com>
2025-05-12 18:42:43 +0900

    fix requestMatchers

--- src/main/java/com/xie/video/config/SecurityConfig.java
+++ src/main/java/com/xie/video/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
         .authorizeHttpRequests(authz -> authz
-        .requestMatchers("/youtube/me").authenticated()
+        .requestMatchers("/youtube/videos").authenticated()
         .anyRequest().permitAll()
         )
         .csrf(csrf -> csrf.disable())
